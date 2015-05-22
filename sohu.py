f48d521be725a1fce3ee163a9a803647707a6ebe
Zhanglln <zhangyuting.yz@gmail.com>
2015-05-22 21:02:34 +0800

    '#'

--- sohu.py
+++ sohu.py
@@ -11,7 +11,7 @@ import urllib2
 from Queue import Queue
 from urlparse import urlparse, urljoin
 from bs4 import BeautifulSoup
-from threading import Thread, RLock
+from threading import Thread, rlock
 
 # 已访问的网址,避免重复
 visited = []
@@ -21,13 +21,13 @@ checked = []
 checked_num = 0
 
 # 定义线程的数量
-WORKS_NUM = 5
+works_num = 5
 
 # 域名,这里最后不能加 / 因为要添加短链接，短链接前面已经有一个 /
-DOMAIN = 'http://m.sohu.com'
+domain = 'http://m.sohu.com'
 
 # 线程锁
-mylock = RLock()
+mylock = rlock()
 
 # 初始化log
 logging.basicConfig(filename = os.path.join(os.getcwd(), time.strftime('%Y-%m-%d-') + 'log.txt'),
@@ -46,17 +46,16 @@ not_check = [None, '#', '/', '#top'
 class CheckThread(Thread):
     def __init__(self, qe, no):
         Thread.__init__(self)
-        self._qe = qe # 队列
-        self._no = no # 第几线程
+        self.qe = qe # 队列
+        self.no = no # 第几线程
         self.threads = 0 # 判断线程的数量
 
     def run(self):
         # 每运行一个线程线程+1
         self.threads += 1
-
         while True:
-            if self._qe.qsize() > 0: # 如果列队中还有Url则继续线程继续执行
-                self._work(self._qe.get(), self._no)
+            if self.qe.qsize() > 0: # 如果列队中还有Url则继续线程继续执行
+                self.work(self.qe.get(), self.no)
             else:
                 # 当线程关闭-1
                 self.threads -= 1
@@ -64,8 +63,7 @@ class CheckThread(Thread):
                 if self.threads == 0:
                     logging.shutdown()
                 break
-
-    def _work(self, url, no):
+    def work(self, url, no):
         check_page_links(url, no)
 
 
@@ -75,7 +73,6 @@ def check_page_links(url, no):
     print '进入页面： %s' % url
     # 提取页面链接
     links = get_all_links(url)
-
     for link in links:
         href = link.get('href')
 
@@ -88,7 +85,6 @@ def check_page_links(url, no):
         whole_url = get_whole_url(href)
         if whole_url in checked:
             continue
-
         mylock.acquire()
         checked.append(whole_url)
         checked_num += 1
@@ -96,12 +92,10 @@ def check_page_links(url, no):
         if checked_num % 2000 == 0:
             time.sleep(5)
         mylock.release()
-
         print '第%s个url' % checked_num
 
         # 检查链接是否可达
         get_url_msg(whole_url)
-
         # 只要 m.sohu.com 域名下的链接
         if whole_url.find('http://m.sohu.com/', 0, 19) == 0:
             # 去重
@@ -111,31 +105,27 @@ def check_page_links(url, no):
                 except Exception, e:
                     write_log('error', whole_url, str(e))
 
-
 # 读取页面链接
 def get_all_links(url):
     text = urllib2.urlopen(url).read()
     soup = BeautifulSoup(text)
     return soup.find_all('a')
 
-
 # 获得完整的url，把短链接前加上 http://m.sohu.com
 def get_whole_url(url):
     uhost = urlparse(url)
     if uhost.netloc == "":
-        url = urljoin(DOMAIN, url)
-
+        url = urljoin(domain, url)
     return url.encode('utf-8')
 
 # 检查链接的有效性并将错误信息写到日志
 def get_url_msg(url):
-#    start_time = time.time()
+# start_time = time.time()
     headers = {
 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36' ,
 'Referer':'http://m.sohu.com'
 }
     req = urllib2.Request(url, headers = headers)
-
     print '\nURL: %s\n' % url
 
     try:
@@ -155,11 +145,8 @@ def get_url_msg(url):
         print '\n' + '*' * 50 + '\n'
         # 写入日志
         write_log('error', url, str(e))
-
     # 防止短时间内链接过于频繁
     time.sleep(1)
-
-
 
 # 写入日志
 def write_log(log_type, url, message):
@@ -168,7 +155,6 @@ def write_log(log_type, url, message):
         logging.error(logger)
     elif log_type == 'warning':
         logging.warning(logger)
-
 
 if __name__ == '__main__':
     # 页面入口 m.sohu.com 导航页面
@@ -183,10 +169,9 @@ if __name__ == '__main__':
     q = Queue(0)
     for column in bd2:
         if column.a:
-            nav_url = DOMAIN + column.a.get('href')
+            nav_url = domain + column.a.get('href')
             q.put(nav_url) # 加入队列
             checked.append(nav_url)
 
-    for i in range(0, WORKS_NUM):
+    for i in range(0, works_num):
         CheckThread(q, i + 1).start()
-
